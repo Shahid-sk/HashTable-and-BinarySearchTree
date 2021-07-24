098c144fb640e97075a446d6212306edff412602
Shahid-sk <mail2sk.shahid@gmail.com>
2021-07-24 13:57:36 +0530

    added: create Binary Search Tree

---
+++ src/binarysearchtree/BinaryMain.java
@@ -0,0 +1,11 @@
+package binarysearchtree;
+
+public class BinaryMain {
+	public static void main(String args[]) {
+		BinarySearchTree<Integer> searchTree = new BinarySearchTree<>();
+		searchTree.insert(56);
+		searchTree.insert(30);
+		searchTree.insert(70);
+	}
+
+}
---
+++ src/binarysearchtree/BinaryNode.java
@@ -0,0 +1,9 @@
+package binarysearchtree;
+
+public class BinaryNode<T>
+{
+	T data;
+	BinaryNode<T> left;
+	BinaryNode<T> right;
+	
+}
